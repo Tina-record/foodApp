15eb9aea461d2967a401b1d849787b0ad2509c17
Tina <tina.1130@outlook.com>
2019-01-25 01:39:29 +0800

    动态渲染界面

--- src/components/address.vue
+++ src/components/address.vue
@@ -8,22 +8,15 @@
             <section class="currentCity">
                 <h4 class="hotCity">当前城市</h4>
                 <div class="defaultCity">
-                    <p v-model="defaultCity"></p>
+                    <p>{{defaultCity}}</p>
                     <p>&gt;</p>
                 </div>
             </section>
             <section class="popularCity">
                 <h4>热门城市</h4>
                 <ul>
-                    <!-- <li v-for="cityArr in cities">{{cityArr.city }}</li> -->
-                    <li>北</li>
-                    <li>上</li>
-                    <li>广</li>
-                    <li>杭</li>
-                    <li>成</li>
-                    <li>重</li>
-                    <li>长</li>
-                    <li>佛</li>
+                    <li v-for="cityArr in cities.slice(0,6)" v-bind:key="cityArr.city">{{cityArr.city}}</li>
+
                 </ul>
             </section>
         </article>
@@ -40,24 +33,44 @@ export default {
     },
     data(){
         return{
-            defaultCity:''        
+            //渲染变量（指拿到数据渲染HTML ）
+            defaultCity:'',
+            cities:''      
         }
     },
     methods:{
         checkCities:function(){
             var cities = 'https://tina-record.github.io/foodApp/v1/cities'
-            console.log(cities);
+
             axios
-                .get(cities)
-                .then(response => (this.decideCities(response)))
+                .get(cities)  //输入地址
+
+                /*then() 指获取数据；
+                response 指获取数据赋值给新变量 ；
+                this 指本文件，也就是创建vue 文件名称（如address.vue）；
+                decideCities() 创建新函数，把获取数据变量传到 decideCities 的实参里，然后decideCities函数进行处理；
+                所以 this.decideCities() 指在本文件（如address.vue）里使用创建函数(decideCities)对数据进行处理。
+                */
+                .then(response => (this.decideCities(response))) 
                 .catch(function (error) {alert("网络无法访问");});
         },
+
+        //对数据进行处理
         decideCities:function(response){
-            console.log(response.data.data)
-             console.log(response.data)
+           
+            //个人理解response对象，有方法、属性。
+            //判断数据当前网络状态
             if(response.data.statu === 200){
                 
                 console.log("suc")
+                
+                //获取当前城市数据
+                this.defaultCity = response.data.data.default.city;
+                // console.log(response.data.data.default.city)
+
+                //获取热门城市们数据
+                this.cities = response.data.data.cities;
+                console.log(this.cities)
 
             }else{
             console.log("服务器出错");
